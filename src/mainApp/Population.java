0d6e24bd7e639e2918f60556d13292c93708902a
khattam <khattam@rose-hulman.edu>
2023-02-15 18:08:14 -0500

    Fixed Erros

--- src/mainApp/Population.java
+++ src/mainApp/Population.java
@@ -9,7 +9,7 @@ import java.util.Random;
  * Functions: Population also sorts populations according to fitness, and generates new 
  * 			  generations
  *  
- * @author lardnece
+ * @author khattam
  *
  */
 public class Population {
@@ -24,7 +24,7 @@ public class Population {
 	private ArrayList<Chromosome> chromes = new ArrayList<Chromosome>();
 
 	/**
-	 * ensures: thisGen Lis t of Chromosomes creates a new random generation of size 100
+	 * ensures: thisGen List of Chromosomes creates a new random generation of size 100
 	 * 
 	 */
 	public void generateRandom(int popSize, int genNum) {
@@ -66,159 +66,166 @@ public class Population {
 //	 * @return count, the number of 1's
 //	 * 
 //	 */
-//	public int fitFunc(Chromosome c) {
-//	
-//		int count = 0;
-//		for(int i =0;i<c.bits.size();i++) {
-//			if(c.bits.get(i)==1)
-//				count++;
-//		}
-//		return count;
-//	}
-//	
-//	/**
-//	 * ensures: (not sure) this function ensures that a separate list called ones 
-//	 * 			holds the fitness count of the chromosomes
-//	 * 
-//	 */
-//	public void createOne() {
-//		for(int i =0;i<thisGen.length;i++) {
-//			ones[i] = fitFunc(thisGen[i]);
-//		}
-//	}
-//	
-//	/**
-//	 * ensures: The chromosomes are sorted based on the number of 1s bits
-//	 * 
-//	 * Checks for 1 bits and swaps the respective chromosome.
-//	 * 		- Chris (Reviewer)
-//	 */
-//	public void bubbleSort() {
-//		Chromosome temp;
-//		int tempInt;
-//		boolean isSorted=false;
-//		while(!isSorted) {
-//			isSorted=true;
-//			for(int i =0;i<ones.length-1;i++) {
-//				if(ones[i]>ones[i+1]) {
-//					tempInt = ones[i];
-//					ones[i]=ones[i+1];
-//					ones[i+1]=tempInt;
-//					
-//					temp=thisGen[i];
-//					thisGen[i] = thisGen[i+1];
-//					thisGen[i+1] = temp;
-//					isSorted=false;	
-//				}
-//			}
-//			
-//		}
-//	}
-//	
-//	/**
-//	 * ensures: the top 50 is mutated in passed into the nextGeneration
-//	 * @param chance, passed in by GUI or default 1
-//	 */
-//	public void evoLoopHelper(double chance) {
-//		createOne();
-//		bubbleSort();
-//		//Passing top 50
-//		for(int i =0;i<50;i++) {
-//			thisGen[i].mutate(chance);
-//			nextGen[i] = thisGen[i];
-//			if(eliteNum!=0)
-//			{
-//				nextGen[i+50] = thisGen[i].copyAndMutate(0);
-//				eliteNum--;
-//			}
-//			else
-//			{
-//				nextGen[i+50] = thisGen[i].copyAndMutate(chance);
-//			}
-//		}
-//	}
-//	
-//	/**
-//	 * ensures: a new generation of 100 chromosomes is created and generated randomly
-//	 * 			and then sorted and mutated
-//	 * @param chance
-//	 * @param generation
-//	 */
-//	public void evoLoop(double chance, int generation) {
-//		generateRandom();
-//		for(int i =1;i<=generation;i++) {
-//			nextGen = new Chromosome[100];
-//			evoLoopHelper(chance);
-//			for(int j =0;i<100;i++){
-//				thisGen[j]=nextGen[j];
-//			}
-//		}
-//	}
-//	//This is another type of fitness function 
-//	public boolean smileyFitness(Chromosome c)
-//	{
-//		int a[]= new int[12];
-//		a[0] = 22;
-//		a[1] = 27;
-//		a[2] = 71;
-//		a[3] = 78;
-//		int k = 4;
-//		for(int i =81;i<=88;i++)
-//		{
-//			a[k] = i;
-//			k++;
-//		}
-//		
-//		//At this we have created the smiley indexes i.e all points where there should be 0s in the smiley
-//		
-//		for(int i =0;i<c.bits.size();i++) {
-//			
-//			if(c.bits.get(i)==0 && isIn(i,a))
-//			{
-//				
-//			}
-//			else
-//			{
-//				return false;
-//			}
-//		}
-//		return true;	
-//	}
-//	
-//	public boolean isIn(int i, int[] a)
-//	{
-//		for(int j = 0;j<a.length;j++)
-//		{
-//			if(a[j]==i)
-//			{
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-//	
-//	//This function checks if the chromosome has more continous 1s than allowed param maxOnes. If it does then it is decrlared unfit
-//	public boolean maxFit(Chromosome c, int maxOnes)
-//	{
-//		int maxCount = 0;
-//		for(int i =0;i<c.bits.size();i++) 
-//		{
-//			if(c.bits.get(i)==1)
-//				maxCount++;
-//			if(maxCount>maxOnes)
-//			{
-//				return false;
-//			}
-//		}
-//		return true;
-//	}
-//	
-//	public void rouletteSelection()
-//	public void elitism(double elitism)
-//	{
-//		eliteNum = elitism;
-//	}
-		
+	public int fitFunc(Chromosome c)
+	{
+		int count = 0;
+		for(int i =0;i<c.bits.size();i++) {
+			if(c.bits.get(i)==1)
+				count++;
+		}
+		return count;
+	}
 	
+	/**
+	 * ensures: (not sure) this function ensures that a separate list called ones 
+	 * 			holds the fitness count of the chromosomes
+	 * 
+	 */
+	public void createOne() {
+		for(int i =0;i<thisGen.length;i++) 
+		{
+			ones[i] = fitFunc(chromes.get(i));
+		}
+	}
+	
+	/**
+	 * ensures: The chromosomes are sorted based on the number of 1s bits
+	 * 
+	 * Checks for 1 bits and swaps the respective chromosome.
+	 * 		- Chris (Reviewer)
+	 */
+	public void bubbleSort() {
+		Chromosome temp;
+		int tempInt;
+		boolean isSorted=false;
+		while(!isSorted) {
+			isSorted=true;
+			for(int i =0;i<ones.length-1;i++) {
+				if(ones[i]>ones[i+1]) {
+					tempInt = ones[i];
+					ones[i]=ones[i+1];
+					ones[i+1]=tempInt;
+					
+					temp=chromes.get(i);
+					chromes.remove(i);
+					chromes.add(i,chromes.get(i+1));
+					chromes.remove(i+1);
+					chromes.add(i+1,temp);
+				}
+			}
+			
+		}
+	}
+	
+	/**
+	 * ensures: the top 50 is mutated in passed into the nextGeneration
+	 * @param chance, passed in by GUI or default 1
+	 */
+	public void evoLoopHelper(double chance) {
+		createOne();
+		bubbleSort();
+		//Passing top 50
+		for(int i =0;i<50;i++) {
+			chromes.get(i).mutate(chance);
+			nextGen[i] = chromes.get(i);
+			if(eliteNum!=0)
+			{
+				nextGen[i+50] = chromes.get(i).copyAndMutate((double) 0);
+				eliteNum--;
+			}
+			else
+			{
+				nextGen[i+50] = chromes.get(i).copyAndMutate(chance);
+			}
+		}
+	}
+	
+	/**
+	 * ensures: a new generation of 100 chromosomes is created and generated randomly
+	 * 			and then sorted and mutated
+	 * @param chance
+	 * @param generation
+	 */
+	public void evoLoop(double chance, int generation,int popSize) {
+		
+		for(int i =1;i<=generation;i++) {
+			generateRandom(popSize, i);
+			nextGen = new Chromosome[chromes.size()];
+			evoLoopHelper(chance);
+			for(int j =0;i<chromes.size();i++)
+			{
+				Chromosome buffer = nextGen[i];
+				chromes.set(i, buffer);
+			}
+		}
+	}
+	//This is another type of fitness function 
+	public boolean smileyFitness(Chromosome c)
+	{
+		int a[]= new int[12];
+		a[0] = 22;
+		a[1] = 27;
+		a[2] = 71;
+		a[3] = 78;
+		int k = 4;
+		for(int i =81;i<=88;i++)
+		{
+			a[k] = i;
+			k++;
+		}
+		
+		//At this we have created the smiley indexes i.e all points where there should be 0s in the smiley
+		
+		for(int i =0;i<c.bits.size();i++) {
+			
+			if(c.bits.get(i)==0 && isIn(i,a))
+			{
+				
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return true;	
+	}
+	
+	public boolean isIn(int i, int[] a)
+	{
+		for(int j = 0;j<a.length;j++)
+		{
+			if(a[j]==i)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//This function checks if the chromosome has more continous 1s than allowed param maxOnes. If it does then it is decrlared unfit
+	public boolean maxFit(Chromosome c, int maxOnes)
+	{
+		int maxCount = 0;
+		for(int i =0;i<c.bits.size();i++) 
+		{
+			if(c.bits.get(i)==1)
+				maxCount++;
+			if(maxCount>maxOnes)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	//Push rouletteSelection
+	public void rouletteSelection()
+	{
+		
+	}
+	public void elitism(double elitism)
+	{
+		eliteNum = elitism;
+	}
 	
 }
